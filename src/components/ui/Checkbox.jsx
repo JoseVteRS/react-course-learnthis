0de071cd9e1d2378c9397c1c167bd214cd872a1a
JoseVteRS <jvrs.90@gmail.com>
2022-04-26 17:10:31 +0200

    checkbox

---
+++ src/components/ui/Checkbox.jsx
@@ -0,0 +1,7 @@
+import style from "./Checkbox.module.css";
+
+const Checkbox = () => {
+	return <input className={style.checkbox} type='checkbox' />;
+};
+
+export default Checkbox;
--- src/components/users/UserList.jsx
+++ src/components/users/UserList.jsx
@@ -1,34 +1,41 @@
-import { filterActiveUsers, filterUsersByName } from "../../lib/filtersAndSorters/users/filters";
+import {
+	filterActiveUsers,
+	filterUsersByName
+} from "../../lib/filtersAndSorters/users/filters";
 import { sortUsers } from "../../lib/filtersAndSorters/users/sorters";
-import { useFilters, useUsers } from "../../lib/hooks"
+import { useFilters, useUsers } from "../../lib/hooks";
 import UsersListFilter from "./UsersListFilter";
 import UsersListRows from "./UsersListRows";
-import style from './UserList.module.css'
+import style from "./UserList.module.css";
 import Button from "../ui/Button";
+import Checkbox from "../ui/Checkbox";
 
 const UserList = ({ initialUsers }) => {
-    const { search, onlyActive, sortBy, ...setFilterFunctions } = useFilters();
-    const { users } = useUsers(initialUsers);
+	const { search, onlyActive, sortBy, ...setFilterFunctions } = useFilters();
+	const { users } = useUsers(initialUsers);
 
-    let usersFiltered = filterActiveUsers(users, onlyActive);
-    usersFiltered = filterUsersByName(usersFiltered, search);
-    usersFiltered = sortUsers(usersFiltered, sortBy);
+	let usersFiltered = filterActiveUsers(users, onlyActive);
+	usersFiltered = filterUsersByName(usersFiltered, search);
+	usersFiltered = sortUsers(usersFiltered, sortBy);
 
-    return (
-        <div className={style.wrapper}>
-            <h1>Listado de usuarios</h1>
-            <UsersListFilter
-                search={search}
-                onlyActive={onlyActive}
-                sortBy={sortBy}
-                {...setFilterFunctions}
-            />
-            <Button type='primary' >Botón</Button>
-            <Button type='secondary' >Botón</Button>
-            <Button type='secondary' disabled  >Botón</Button>
-            <UsersListRows users={usersFiltered} />
-        </div>
-    )
-}
+	return (
+		<div className={style.wrapper}>
+			<h1>Listado de usuarios</h1>
+			<UsersListFilter
+				search={search}
+				onlyActive={onlyActive}
+				sortBy={sortBy}
+				{...setFilterFunctions}
+			/>
+			<Button type='primary'>Botón</Button>
+			<Button type='secondary'>Botón</Button>
+			<Button type='secondary' disabled>
+				Botón
+			</Button>
+			<Checkbox />
+			<UsersListRows users={usersFiltered} />
+		</div>
+	);
+};
 
-export default UserList
+export default UserList;
